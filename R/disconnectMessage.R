8904776a1a0305e7e61f9cfa93660480ad61464a
Dean Attali <daattali@gmail.com>
2020-06-08 13:30:09 -0400

    add documentation

--- R/disconnectMessage.R
+++ R/disconnectMessage.R
@@ -1,5 +1,38 @@
-#' use stopApp() to test
-#' refresh = "" fo no button
+#' Show a nice message when a shiny app disconnects
+#'
+#' A shiny app can disconnect for a variety of reasons: an unrecoverable error occurred in
+#' the app, the server went down, the user's internet connection died, or any other reason
+#' that might cause the shiny app to lose connection to its server. Call `disonnectMessage()`
+#' anywhere in a Shiny app's UI to add a nice message when this happens. The message works both
+#' locally (running Shiny apps within RStudio) and on Shiny servers (such as shinyapps.io,
+#' RStudio Connect, Shiny Server Open Source, Shiny Server Pro).
+#'
+#' @param text The text to show in the message.
+#' @param refresh The text to show in a link that allows the user to refresh the page.
+#' Use `refresh = ""` if you don't want to show a refresh link.
+#' @param size The size of the message, one of "s", "m", "l" (small, medium, or large).
+#' @param background The background colour of the message box.
+#' @param colour The colour of the text of the message box.
+#' @param overlayColour The colour of the overlay to draw on the page behind the message box.
+#' An overlay is used to "grey out" the application and draw attention to the message. Use
+#' `overlayOpacity = 0` to disable the overlay.
+#' @param overlayOpacity The opacity of the overlay, from 0 (fully transparent) to 1
+#' (fully opaque). Use `overlayOpacity = 0` to disable the overlay.
+#' @examples
+#' if (interactive()) {
+#'   library(shiny)
+#'   shinyApp(
+#'     ui = fluidPage(
+#'       disconnectMessage(),
+#'       actionButton("disconnect", "Disconnect the app")
+#'     ),
+#'     server = function(input, output) {
+#'       observeEvent(input$disconnect, {
+#'         session$close()
+#'       })
+#'     }
+#'   )
+#' }
 #' @export
 disconnectMessage <- function(
   text = "An error occured. Please refresh the page and try again.",
@@ -34,75 +67,89 @@ disconnectMessage <- function(
   }
 
   htmltools::tagList(
-    if (Sys.getenv("SHINY_PORT", "") == "")
-      htmltools::tagList(
-        htmltools::tags$script("
-                               $(function() {
-$(document).on('shiny:disconnected', function(event) {
-$('#ss-connect-dialog').show();
-$('#ss-overlay').show();
-})
-});
-                               "),
-        htmltools::tags$div(id="ss-connect-dialog", style="display: none;",
-                            htmltools::tags$a("zzz",id="ss-reload-link", href="#", onclick="window.location.reload(true);")),
-        htmltools::tags$div(id="ss-overlay", style="display: none;")
-      )
-    else
-      NULL,
-
+    getLocalTags(),
     htmltools::tags$head(
       htmltools::tags$style(
         glue::glue(
           .open = "{{", .close = "}}",
+
           "#shiny-disconnected-overlay { display: none !important; }",
+
           "#ss-overlay {
-            background-color: {{overlayColour}} !important;
-            opacity: {{overlayOpacity}} !important;
-            position: fixed !important;
-            top: 0 !important;
-            left: 0 !important;
-            bottom: 0 !important;
-            right: 0 !important;
-            z-index: 99998 !important;
-            overflow: hidden !important;
-            cursor: not-allowed !important;
+             background-color: {{overlayColour}} !important;
+             opacity: {{overlayOpacity}} !important;
+             position: fixed !important;
+             top: 0 !important;
+             left: 0 !important;
+             bottom: 0 !important;
+             right: 0 !important;
+             z-index: 99998 !important;
+             overflow: hidden !important;
+             cursor: not-allowed !important;
           }",
+
           "#ss-connect-dialog {
-            background: {{background}} !important;
-            color: {{colour}} !important;
-            width: {{width}}px !important;
-            margin-left: -{{width/2}}px !important;
-            font-size: {{font}}px !important;
-            position: fixed !important;
-            top: 50px !important;
-            bottom: auto !important;
-            left: 50% !important;
-            padding: 1em 1.5em !important;
-            text-align: center !important;
-            height: auto !important;
-            opacity: 1 !important;
-            z-index: 99999 !important;
-            border-radius: 3px !important;
-            box-shadow: rgba(0, 0, 0, 0.3) 3px 3px 10px !important;
+             background: {{background}} !important;
+             color: {{colour}} !important;
+             width: {{width}}px !important;
+             margin-left: -{{width/2}}px !important;
+             font-size: {{font}}px !important;
+             position: fixed !important;
+             top: 50px !important;
+             bottom: auto !important;
+             left: 50% !important;
+             padding: 1em 1.5em !important;
+             text-align: center !important;
+             height: auto !important;
+             opacity: 1 !important;
+             z-index: 99999 !important;
+             border-radius: 3px !important;
+             box-shadow: rgba(0, 0, 0, 0.3) 3px 3px 10px !important;
           }",
-          "#ss-connect-dialog::before { content: '{{text}}'}",
-          "#ss-connect-dialog label {
-            display: none !important;
-          }
-          #ss-connect-dialog a {
-            display: {{ if (refresh == '') 'none' else 'block' }} !important;
-            font-size: 0 !important;
-            margin-top: {{font}}px !important;
-            font-weight: normal !important;
-          }
-          #ss-connect-dialog a::before {
+
+          "#ss-connect-dialog::before { content: '{{text}}' }",
+
+          "#ss-connect-dialog label { display: none !important; }",
+
+          "#ss-connect-dialog a {
+             display: {{ if (refresh == '') 'none' else 'block' }} !important;
+             font-size: 0 !important;
+             margin-top: {{font}}px !important;
+             font-weight: normal !important;
+          }",
+
+          "#ss-connect-dialog a::before {
             content: '{{refresh}}';
             font-size: {{font}}px;
-          }
-          "
+          }"
         )
       )
     )
   )
+}
+
+getLocalTags <- function() {
+  if (!isLocal()) {
+    return(NULL)
+  }
+
+  htmltools::tagList(
+    htmltools::tags$script(paste0(
+      "$(function() {",
+      "  $(document).on('shiny:disconnected', function(event) {",
+      "    $('#ss-connect-dialog').show();",
+      "    $('#ss-overlay').show();",
+      "  })",
+      "});"
+    )),
+    htmltools::tags$div(
+      id="ss-connect-dialog", style="display: none;",
+      htmltools::tags$a(id="ss-reload-link", href="#", onclick="window.location.reload(true);")
+    ),
+    htmltools::tags$div(id="ss-overlay", style="display: none;")
+  )
+}
+
+isLocal <- function() {
+  Sys.getenv("SHINY_PORT", "") == ""
 }
--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# shinydisconnect
+# shinydisconnect - Show a nice message when a Shiny app disconnects or errors
+
+<a href="https://cran.r-project.org/package=shinydisconnect">
+<img src="https://www.r-pkg.org/badges/version/shinydisconnect" alt="CRAN version" />
+</a>
+
+> Copyright 2020 Dean Attali. Licensed under the MIT license.
+
+## Overview
+
+A shiny app can disconnect for a variety of reasons: an unrecoverable error occurred in the app, the server went down, the user's internet connection died, or any other reason that might cause the shiny app to lose connection to its server.
+
+`shinydisconnect` allows you to add a nice message to the user when the app disconnects.  The message works both locally (running Shiny apps within RStudio) and on Shiny servers (such as shinyapps.io, RStudio Connect, Shiny Server Open Source, Shiny Server Pro).
+
+**If you find shinydisconnect useful, please consider supporting my work\!**
+
+<p align="center">
+
+<a style="display: inline-block;" href="https://paypal.me/daattali">
+<img height="35" src="https://camo.githubusercontent.com/0e9e5cac101f7093336b4589c380ab5dcfdcbab0/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f74776f6c66736f6e2f70617970616c2d6769746875622d627574746f6e40312e302e302f646973742f627574746f6e2e737667" />
+</a>
+<a style="display: inline-block; margin-left: 10px;" href="https://github.com/sponsors/daattali">
+<img height="35" src="https://i.imgur.com/034B8vq.png" /> </a>
+
+</p>
+
+## Installation
+
+The package is not yet on CRAN, so to install use these comments:
+
+```
+install.packages("remotes")
+remotes::install_github("daattali/shinydisconnect")
+```
+
+## How to use
+
+Call `disonnectMessage()` anywhere in a Shiny app's UI to add a nice message when this happens.
+
+Without using this package, a shiny app that disconnects will either just show a greyed out screen if running locally (with no message), or will show a small message in the bottom-left corner that you cannot modify when running in a server.
+
+Example basic usage:
+
+```
+library(shiny)
+shinyApp(
+  ui = fluidPage(
+    disconnectMessage(),
+    actionButton("disconnect", "Disconnect the app")
+  ),
+  server = function(input, output) {
+    observeEvent(input$disconnect, {
+      session$close()
+    })
+  }
+)
+```
+
+![basic screenshot](inst/img/basic.PNG)
+
+Example advanced usage:
+
+```
+shinyApp(
+  ui = fluidPage(
+    disconnectMessage(
+      text = "Your session timed out, reload the application.",
+      refresh = "Reload now",
+      background = "#f89f43",
+      colour = "white",
+      overlayColour = "grey",
+      overlayOpacity = 0.3
+    ),
+    actionButton("disconnect", "Disconnect the app")
+  ),
+  server = function(input, output) {
+    observeEvent(input$disconnect, {
+      session$close()
+    })
+  }
+)
+```
+
+![advanced screenshot](inst/img/advanced.PNG)
